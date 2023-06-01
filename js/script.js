b138ed055e1c696a9b5cb952b1e4b04708b6ee11
AhmedNaoum-dev <raoufnaoum15@gmail.com>
2023-06-01 13:31:47 +0100

    edit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+let button = document.getElementById("humberger");
+let men = document.getElementById("menu");
+
+// button.addEventListener("click", dropmenu);
+// function dropmenu(){
+//     if(men.style.display == "none"){
+//         men.style.display = "flex";
+//     }else{
+//         men.style.display = "none";
+//     }
+// };
+button.onclick = ()=>{
+    if(men.style.display == "flex"){
+        men.style.display = "none";
+    }else{
+        men.style.display = "flex";
+    }
+};
